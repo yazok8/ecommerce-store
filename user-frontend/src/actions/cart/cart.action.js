8d03f426fc80a18d5854191c96523567cd6b57f6
yazan <yazankhirfan@gmail.com>
2021-08-31 22:28:32 -0400

    add cartItem, increment and decrement functions

--- user-frontend/src/actions/cart/cart.action.js
+++ user-frontend/src/actions/cart/cart.action.js
@@ -9,12 +9,12 @@ import {
 } from './cart.types'
 import store from '../../store'
 
-export const addToCart = (product) => {
+export const addToCart = (product, newQty = 1) => {
   return async (dispatch) => {
     const { cartItems } = store.getState().cart
     // console.log("action::products", cartItems);
     const qty = cartItems[product._id]
-      ? parseInt(cartItems[product._id].qty + 1)
+      ? parseInt(cartItems[product._id].qty + newQty)
       : 1
 
     cartItems[product._id] = {
--- user-frontend/src/containers/cartpage/CartPage.js
+++ user-frontend/src/containers/cartpage/CartPage.js
@@ -1,34 +1,49 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import Layout from '../../components/layout/Layout'
 import Card from '../../components/UI/card/Card'
+import CartItem from './cartitem/CartItem'
+import { addToCart } from '../../actions/actions'
 import './CartPage.style.css'
 
 const CartPage = (props) => {
   const dispatch = useDispatch()
   const cart = useSelector((state) => state.cart)
-  const cartItems = cart.cartItems
+  // const cartItems = cart.cartItems
+
+  const [cartItems, setCartItems] = useState(cart.cartItems)
+
+  useEffect(() => {
+    setCartItems(cart.cartItems)
+  }, [cart.cartItems])
+
+  const onQuantityIncrement = (_id, qty) => {
+    //console.log(_id, qty)
+    const { name, price, img } = cartItems[_id]
+    dispatch(addToCart({ _id, name, price, img }, 1))
+  }
+
+  const onQuantityDecrement = (_id, qty) => {
+    const { name, price, img } = cartItems[_id]
+    dispatch(addToCart({ _id, name, price, img }, -1))
+  }
 
   return (
     <Layout>
-      <div className="cartContainer">
+      <div className="cartContainer" style={{ alignItems: 'flex-start' }}>
         <Card headerLeft={`My Cart`} headerRight={<div>Deliver to </div>}>
           {Object.keys(cartItems).map((key, index) => (
-            <div key={index} className="flexRow">
-              <div className="cartProductContainer">
-                <img src="" />
-              </div>
-              <div className="cartItemDetails">
-                <div>
-                  {cartItems[key].name} - qty - {cartItems[key].qty}
-                </div>
-                <div>Delivery in 3 - 5 days</div>
-              </div>
-            </div>
+            <CartItem
+              key={index}
+              cartItem={cartItems[key]}
+              onQuantityIncr={onQuantityIncrement}
+              onQuantityDecr={onQuantityDecrement}
+            />
           ))}
+
           {/* {JSON.stringify(cartItems)} */}
         </Card>
-        <Card style={{ width: '500px' }}>Price</Card>
+        <Card headerLeft="Price" style={{ width: '500px' }}></Card>
       </div>
     </Layout>
   )
